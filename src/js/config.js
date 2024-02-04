0868e7662d99cac2c57af4cc9e5d77328eb6a0db
Messinias1 <41557968+Messinias1@users.noreply.github.com>
2024-02-04 14:46:54 -0500

    another deploy prep

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ee1cde53-17cd-49a5-93c2-d09a1324c3be';
+export const MODAL_CLOSE_SEC = 2.5;
